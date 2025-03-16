ec4f281798b9f517ee659307a7a3c18460fbd618
Aman Singh <amansingh09122004@gmail.com>
2025-03-16 21:44:47 +0530

    created database

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/models/ques_model.dart';
 import './screens/home_screen.dart';
 import './models/db_connect.dart';
 
 void main() {
+  var db = DBConnect();
+  db.addQuestion(
+    Question(id: '3', 
+    title: 'what is 45 x 25 ?', 
+    options: {
+      '2245':false,
+      '4718':false,
+      '1125':true,
+      '4871':false,
+    })
+  );
   runApp(const MyApp());
 }
 
@@ -12,7 +24,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-
       debugShowCheckedModeBanner: false,
 
       home: HomeScreen(),
